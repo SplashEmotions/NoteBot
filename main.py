70b0c8636806f4ff3775cd6d34f8b195645f0f0d
SplashEmotions <smertvam6@gmail.com>
2023-05-21 16:09:38 +0600

    idkkkk

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("123")
+import configparser
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+import discord
+
+config = configparser.ConfigParser()
+config.read('config.txt')
+
+discord_token = config.get('DEFAULT', 'discord_token')
+
+Client = discord.Client()
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print("Установка активности!") # увед. о начале работы
+
+bot.run(discord_token) #суда ваш токен

152f327a0645b6da09190ebb86e3628cc1c3eb61
SplashEmotions <smertvam6@gmail.com>
2023-05-21 23:57:36 +0600

    idk

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-import configparser
-from discord.ext.commands import Bot
-from discord.ext import commands
-import asyncio
-import time
 import discord
+import configparser
+from discord.ext import commands
 
 config = configparser.ConfigParser()
 config.read('config.txt')
 
-discord_token = config.get('DEFAULT', 'discord_token')
+discord_token:str = config.get('DEFAULT', 'discord_token')
 
-Client = discord.Client()
-bot = commands.Bot(command_prefix='!')
+activity_text = "SplashBot"
+
+intents = discord.Intents.default()
+intents.presences = True
+intents.activities = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
-    print("Установка активности!") # увед. о начале работы
+    print(f'{bot.user.name} включен')
 
-bot.run(discord_token) 
+    activity = discord.Game(name=activity_text)
+    await bot.change_presence(activity=activity)
+
+bot.run(discord_token)

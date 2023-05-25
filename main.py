b16a86a526472f1dc90c578fbc3c637e21ace66a
SplashEmotions <smertvam6@gmail.com>
2023-05-25 23:48:17 +0600

    work version

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import disnake
 from disnake import OptionType, OptionChoice
 from disnake.ext import commands
 import pytz
-from datetime import datetime
+from datetime import datetime, timedelta
 
 config = configparser.ConfigParser()
 config.read('config.txt')
@@ -27,28 +27,43 @@ async def on_ready():
     print(f'{bot.user.name} включен')
 
 @bot.command()
-async def info(ctx):
-    await ctx.send('Введите время:')
+async def schedule(ctx):
+    await ctx.send('Введите время в формате ЧЧ:ММ (24-часовой формат):')
     time_msg = await bot.wait_for('message', check=lambda m: m.author == ctx.author)
-    time_info = time_msg.content
+    time_str = time_msg.content
 
-    await ctx.send('Введите дату:')
+    await ctx.send('Введите дату в формате ДД-ММ-ГГГГ:')
     date_msg = await bot.wait_for('message', check=lambda m: m.author == ctx.author)
-    date_info = date_msg.content
+    date_str = date_msg.content
 
-    await ctx.send('Введите текст:')
-    text_msg = await bot.wait_for('message', check=lambda m: m.author == ctx.author)
-    text_info = text_msg.content
+    await ctx.send('Введите текст операции:')
+    operation_msg = await bot.wait_for('message', check=lambda m: m.author == ctx.author)
+    operation_text = operation_msg.content
 
-    await ctx.send(f'Заметка сохранена:\nВремя: {time_info}\nДата: {date_info}\nТекст: {text_info}')
+    await ctx.send(f'Операция запланирована на {time_str} {date_str}')
 
-    # Получаем текущее время и дату в часовом поясе России
-    current_datetime = datetime.now(pytz.timezone('Europe/Moscow'))
-    current_time = current_datetime.strftime('%H:%M:%S')
-    current_date = current_datetime.strftime('%Y-%m-%d')
+    # Преобразуем время и дату в объект datetime
+    scheduled_time = datetime.strptime(f'{date_str} {time_str}', '%d-%m-%Y %H:%M')
 
-    # Проверяем, совпадает ли текущее время и дата с заданными пользователем
-    if current_time == time_info and current_date == date_info:
-        await ctx.send(text_info)
+    # Функция, которая будет выполнена, когда наступит указанное время и дата
+    async def execute_operation():
+        await ctx.send(f'Операция выполнена: {operation_text}')
+
+    # Функция для обновления времени по часовому поясу России
+    async def update_time():
+        while True:
+            current_time = datetime.now(pytz.timezone('Europe/Moscow')).strftime('%H:%M:%S')
+            await bot.change_presence(activity=disnake.Game(name=f"Current time in Russia (Moscow): {current_time}"))
+            await disnake.utils.sleep_until(datetime.now() + timedelta(minutes=1))
+
+    # Запускаем задачу обновления времени
+    bot.loop.create_task(update_time())
+
+    # Вычисляем время до выполнения операции
+    delta = scheduled_time - datetime.now()
+
+    # Запускаем таймер и выполняем операцию по истечении времени
+    bot.loop.create_task(disnake.utils.sleep(delta.total_seconds()))
+    bot.loop.create_task(execute_operation())
 
 bot.run(discord_token)

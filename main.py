23f7987dd5cb29402d848cbcee0c5239c5f05226
SplashEmotions <smertvam6@gmail.com>
2023-05-26 19:48:44 +0600

    all

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ async def on_ready():
     print(f'{bot.user.name} включен')
 
 @bot.command()
-async def schedule(ctx):
+async def note(ctx):
     await ctx.send('Введите время в формате ЧЧ:ММ (24-часовой формат):')
     time_msg = await bot.wait_for('message', check=lambda m: m.author == ctx.author)
     time_str = time_msg.content
@@ -36,25 +36,25 @@ async def schedule(ctx):
     date_msg = await bot.wait_for('message', check=lambda m: m.author == ctx.author)
     date_str = date_msg.content
 
-    await ctx.send('Введите текст операции:')
+    await ctx.send('Введите текст упоминания:')
     operation_msg = await bot.wait_for('message', check=lambda m: m.author == ctx.author)
     operation_text = operation_msg.content
 
-    await ctx.send(f'Операция запланирована на {time_str} {date_str}')
+    await ctx.send(f'Упоминание запланировано на {time_str} {date_str}')
 
     # Преобразуем время и дату в объект datetime
     scheduled_time = datetime.strptime(f'{date_str} {time_str}', '%d-%m-%Y %H:%M')
 
     # Функция, которая будет выполнена, когда наступит указанное время и дата
     async def execute_operation():
-        await ctx.send(f'Операция выполнена: {operation_text}')
+        await ctx.send(f'{ctx.author.mention} Упоминание выполнено: {operation_text}')
 
     # Функция для обновления времени по часовому поясу России
     async def update_time():
         while True:
-            current_time = datetime.now(pytz.timezone('Europe/Moscow')).strftime('%H:%M:%S')
-            await bot.change_presence(activity=disnake.Game(name=f"Current time in Russia (Moscow): {current_time}"))
-            await asyncio.sleep(60)  # Ожидаем 60 секунд
+            current_time = datetime.now(pytz.timezone('Europe/Moscow')).strftime('%H:%M')
+            await bot.change_presence(activity=disnake.Activity(type=disnake.ActivityType.watching, name=f"Время МСК: {current_time}"))
+            await asyncio.sleep(10)  # Ожидаем 10 секунд
 
     # Запускаем задачу обновления времени
     bot.loop.create_task(update_time())
